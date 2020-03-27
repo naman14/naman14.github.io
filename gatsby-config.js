828e0cbcf71ee5ed39116019a821d9f95246417f
Naman Dwivedi <namandwivedi14@gmail.com>
2020-03-27 23:38:51 +0530

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,15 @@ module.exports = {
     author: `@naman14`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-60857131-4",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true
+      }
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
